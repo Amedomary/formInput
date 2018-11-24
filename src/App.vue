599ef74a43bb76a7d519e1116ae4b0c1a664b799
Amed <amedomary@mail>
2018-11-25 00:47:32 +0400

    start js

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <div id="app">
+  <div id='app'>
     <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang='scss'>
   :root {
     --white: #c8c8c8;
     --black: #282828;
@@ -47,7 +47,7 @@ export default {
   body {
     min-height: 100%;
     height: 100%;
-    font-size: 45px;
+    font-size: 25px;
     margin: 0;
   }
 
@@ -73,45 +73,45 @@ export default {
   // 'Poiret One', cursive;
 
   // Шрифты
-  @import url("https://fonts.googleapis.com/css?family=Aclonica|Amatic+SC|Bangers|Black+Han+Sans|Dancing+Script|Dokdo|Gamja+Flower|Gugi|Indie+Flower|Italianno|Lobster|Pattaya|Poiret+One|Rubik+Mono+One|Sigmar+One");
+  @import url('https://fonts.googleapis.com/css?family=Aclonica|Amatic+SC|Bangers|Black+Han+Sans|Dancing+Script|Dokdo|Gamja+Flower|Gugi|Indie+Flower|Italianno|Lobster|Pattaya|Poiret+One|Rubik+Mono+One|Sigmar+One');
 
   @font-face {
-    font-family: "anvylregular";
-    src: url("assets/fonts/anvyl-webfont.eot");
-    src: url("assets/fonts/anvyl-webfont.eot?#iefix") format("embedded-opentype"),
-      url("assets/fonts/anvyl-webfont.woff2") format("woff2"),
-      url("assets/fonts/anvyl-webfont.ttf") format("truetype"),
-      url("assets/fonts/anvyl-webfont.svg#anvylregular") format("svg");
+    font-family: 'anvylregular';
+    src: url('assets/fonts/anvyl-webfont.eot');
+    src: url('assets/fonts/anvyl-webfont.eot?#iefix') format('embedded-opentype'),
+      url('assets/fonts/anvyl-webfont.woff2') format('woff2'),
+      url('assets/fonts/anvyl-webfont.ttf') format('truetype'),
+      url('assets/fonts/anvyl-webfont.svg#anvylregular') format('svg');
     font-weight: normal;
     font-style: normal;
   }
 
   @font-face {
-    font-family: "polar_vertexregular";
-    src: url("assets/fonts/polarvertex-regular-webfont.eot");
-    src: url("assets/fonts/polarvertex-regular-webfont.eot?#iefix") format("embedded-opentype"),
-      url("assets/fonts/polarvertex-regular-webfont.woff2") format("woff2"),
-      url("assets/fonts/polarvertex-regular-webfont.ttf") format("truetype"),
-      url("assets/fonts/polarvertex-regular-webfont.svg#polar_vertexregular") format("svg");
+    font-family: 'polar_vertexregular';
+    src: url('assets/fonts/polarvertex-regular-webfont.eot');
+    src: url('assets/fonts/polarvertex-regular-webfont.eot?#iefix') format('embedded-opentype'),
+      url('assets/fonts/polarvertex-regular-webfont.woff2') format('woff2'),
+      url('assets/fonts/polarvertex-regular-webfont.ttf') format('truetype'),
+      url('assets/fonts/polarvertex-regular-webfont.svg#polar_vertexregular') format('svg');
     font-weight: normal;
     font-style: normal;
   }
 
   body {
-    font-family: "Lobster", cursive;
-    font-family: "Amatic SC", cursive;
-    font-family: "Poiret One", cursive;
-    font-family: "Pattaya", sans-serif;
-    font-family: "Rubik Mono One", sans-serif;
-    font-family: "Black Han Sans", sans-serif;
-    font-family: "Indie Flower", cursive;
-    font-family: "Dancing Script", cursive;
-    font-family: "Sigmar One", cursive;
-    font-family: "Bangers", cursive;
-    font-family: "Italianno", cursive;
-    font-family: "Dokdo", cursive;
-    font-family: "Gamja Flower", cursive;
-    font-family: "Gugi", cursive;
-    font-family: "Aclonica", sans-serif;
+    font-family: 'Lobster', cursive;
+    font-family: 'Amatic SC', cursive;
+    font-family: 'Poiret One', cursive;
+    font-family: 'Pattaya', sans-serif;
+    font-family: 'Rubik Mono One', sans-serif;
+    font-family: 'Black Han Sans', sans-serif;
+    font-family: 'Indie Flower', cursive;
+    font-family: 'Sigmar One', cursive;
+    font-family: 'Bangers', cursive;
+    font-family: 'Italianno', cursive;
+    font-family: 'Dokdo', cursive;
+    font-family: 'Gamja Flower', cursive;
+    font-family: 'Gugi', cursive;
+    font-family: 'Aclonica', sans-serif;
+    font-family: 'Dancing Script', cursive;
   }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,68 @@
 <template>
-  <main class="wrapper">
+  <main class='wrapper'>
     <header>
-      <p class="heading">Login</p>
-      <p class="create">
-        <a href="#">Create account</a>
+      <p class='heading'>Login</p>
+      <p class='create'>
+        <a href='#'>Create account</a>
       </p>
     </header>
 
-    <section class="input__row">
-      <div class="input__cell">
-        <label class="input__wrap">
-          <p class="username username--label">Username</p>
-          <input class="username username--input" type="text" value="" placeholder="">
+    <section class='avatar__section'>
+      <transition name="avatar">
+        <div class="avatar__row" v-if="state.userIsFound">
+          <img class='avatar__pic' src='@/assets/ava.jpg' alt=''>
+          <p class='avatar__text' >Wellcome back, Mila</p>
+        </div>
+      </transition>
+    </section>
+
+    <section class='input__row'>
+      <div class='input__cell'>
+        <label class='input__wrap'>
+          <p class='username username--label'
+             :class="state.unFocus ? 'focus' : ''"
+          > Username </p>
+          <input class='username username--input' type='text' value='' placeholder=''
+                 ref="username_input"
+                 v-on:focus="focusUser()"
+                 v-on:blur="blurUser()"
+                 v-on:input="inputUser()"
+          >
         </label>
       </div>
-      <hr class="input__separator" />
-      <div class="input__cell">
-        <label class="input__wrap">
-          <p class="password password--label">Password</p>
-          <input class="password password--input" type="password" value="" placeholder="">
+      <hr class='input__separator'
+          :class="state.searchUser ? 'search' : ''"
+      />
+      <div class='input__cell'>
+        <label class='input__wrap'>
+          <transition name="password">
+            <p class='password password--label'
+              v-if="state.userIsFound"
+            >Password</p>
+          </transition>
+          <input class='password password--input' type='password' value='' placeholder=''
+             v-if="state.userIsFound"
+          >
         </label>
       </div>
     </section>
 
+    <section class="action__row">
+      <div class="action__cell action__cell--next">
+        <a class="action__link action__link--next" href="#" v-if="false">
+          Next
+        </a>
+      </div>
+      <div class="action__cell action__cell--singin">
+        <a class="action__link action__link--singin" href="#" v-if="false">
+          Sing in
+        </a>
+      </div>
+    </section>
+
     <footer>
-      <p class="create">
-        <a href="#">Fargot password?</a>
+      <p class='create'>
+        <a href='#'>Forgot password?</a>
       </p>
     </footer>
   </main>
@@ -33,105 +70,336 @@
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      state: {
+        // un - user name
+        // pw - pass word
+        unFocus: false,
+        unComplete: false,
+        pwFocus: false,
+        pwComplete: false,
+        searchUser: false,
+        userIsFound: false,
+      }
+    };
+  },
+  methods: {
+    focusUser() {
+      console.log(this);
+      console.log(this.state.unFocus);
+      
+      this.state.unFocus = true;
+    },
+
+    blurUser() {
+      // this.state.unFocus = false;
+    },
+
+    inputUser() {
+      clearTimeout(this.timerId);
+      if (this.$refs.username_input.value !== '') {
+        this.timerId = setTimeout(() => {
+          this.searchUser();
+        }, 1000);
+      }
+    },
+
+    searchUser() {
+      this.state.searchUser = true;
+      setTimeout(() => {
+        this.state.searchUser = false;
+        this.state.userIsFound = true;
+      }, 3000);
+      
+    }
+  },
+  mounted() {
+  }
+
 };
 </script>
 
-<style scoped lang="scss">
-  .wrapper {
-    margin: auto;
+<style scoped lang='scss'>
+.wrapper {
+  margin: auto;
+  width: 100%;
+  padding: 24px;
+}
+
+header {
+  margin-bottom: 20px;
+}
+
+footer {
+  margin-bottom: 100px;
+}
+
+.heading {
+  text-align: center;
+  font-size: 55px;
+  font-family: "polar_vertexregular";
+}
+
+.avatar {
+  &__section {
+    height: 70px;
+    margin-bottom: 20px;
+  }
+
+  &__row {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__pic {
+    height: 70px;
+    width: 70px;
+    display: block;
+    border-radius: 75%;
+    margin-right: 13px;
+    object-fit: cover;
+  }
+
+  &__text {
+    font-family: "Italianno", cursive;
+    font-size: 45px;
+    color: #ddd;
+  }
+}
+
+.create {
+  font-size: 25px;
+  font-family: "Dancing Script", cursive;
+  text-align: center;
+  line-height: 30px;
+}
+
+.username {
+  font-family: "anvylregular";
+  font-size: 110px;
+  text-align: right;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  line-height: 113px;
+  height: 113px;
+  padding-right: 15px;
+
+  &--label {
+    cursor: pointer;
+    opacity: 0.35;
+    transition: 1s;
+
+    &.focus {
+      opacity: 0;
+    }
+  }
+
+  &--input {
+    color: #fff;
+  }
+}
+
+.password {
+  font-family: "anvylregular";
+  font-size: 110px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  line-height: 113px;
+  height: 113px;
+  padding-left: 15px;
+
+  &--label {
+    cursor: pointer;
+    opacity: 0.35;
+  }
+
+  &--input {
+    color: #fff;
+  }
+}
+
+.input {
+  &__row {
+    display: flex;
+    flex-flow: row nowrap;
     width: 100%;
-    padding: 24px;
+    justify-content: center;
+    align-items: stretch;
+    margin-bottom: 25px;
   }
 
-  header {
-    margin-bottom: 100px;
+  &__cell {
+    flex: 0 0 auto;
+    width: 50%;
   }
 
-  footer {
-    margin-bottom: 100px;
-  }
+  &__separator {
+    display: inline-block;
+    flex: 0 0 auto;
+    width: 3px;
+    height: 124px;
+    background-color: var(--white);
 
-  .heading {
-    text-align: center;
-    font-size: 55px;
-    font-family: "polar_vertexregular";
-  }
-
-  .create {
-    font-size: 25px;
-    font-family: "Dancing Script", cursive;
-    text-align: center;
-  }
-
-  .username {
-    font-family: "anvylregular";
-    font-size: 92px;
-    text-align: right;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    left: 0;
-
-    &--label {
-      cursor: pointer;
-      opacity: .3;
-    }
-
-    &--input {
-      color: #ddd;
-    }
-  }
-
-  .password {
-    font-family: "anvylregular";
-    font-size: 92px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-
-    &--label {
-      cursor: pointer;
-      opacity: .3;
-    }
-
-    &--input {
-      color: #ddd;
-    }
-  }
-
-  .input {
-    &__row {
-      display: flex;
-      flex-flow: row nowrap;
-      width: 100%;
-      justify-content: center;
-      align-items: stretch;
-      margin-bottom: 100px;
-    }
-
-    &__cell {
-      flex: 0 0 auto;
-      width: 50%;
-    }
-
-    &__separator {
-      display: inline-block;
-      flex: 0 0 auto;
-      width: 3px;
-      height: 124px;
-      margin: 0 15px;
-      background-color: var(--white);
-    }
-
-    &__wrap {
-      display: block;
-      width: 100%;
-      height: 100%;
+    &.search {
+      overflow: hidden;
       position: relative;
+
+      &:after {
+        content: '';
+        position: absolute;
+        display: block;
+        left: 0;
+        right: 0;
+        bottom: -60px;
+        height: 60px;
+        background-color: var(--accent);
+        animation: separator_search  1.5s infinite linear;
+      }
     }
   }
+
+  &__wrap {
+    display: block;
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+}
+
+.action {
+  &__row {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: stretch;
+    margin-bottom: 25px;
+    height: 66px;
+  }
+
+  &__cell {
+    flex: 0 0 auto;
+    width: 50%;
+    padding: 0 15px;
+
+    &--next {
+      text-align: right;
+    }
+  }
+
+  &__link {
+    font-family: "Italianno", cursive;
+    font-size: 45px;
+    display: inline-block;
+    padding: 5px;
+    position: relative;
+    transition: color 0.1s;
+
+    &::after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 1px;
+      background-image: linear-gradient(
+        to right,
+        rgba(255, 255, 255, 0),
+        #ddd,
+        #ddd,
+        #ddd,
+        rgba(255, 255, 255, 0)
+      );
+      bottom: 17px;
+      left: 0;
+    }
+
+    &:hover {
+      &:after {
+        color: var(--accent);
+        background-image: linear-gradient(
+          to right,
+          rgba(255, 255, 255, 0),
+          var(--accent),
+          #ddd,
+          #ddd,
+          rgba(255, 255, 255, 0)
+        );
+      }
+    }
+  }
+}
+
+
+// Анимации
+.avatar {
+  &-enter-active {
+    transition: 1s;
+    & * {
+      transition: 1s;
+    }
+  }
+  &-enter {
+    & .avatar__pic {
+      z-index: 25;
+      opacity: 0;
+      transform: translateX(150%);
+    }
+    & .avatar__text {
+      z-index: 5;
+      opacity: 0;
+      transform: scale(.9);
+    }
+  }
+  &-enter-to {
+    & .avatar__pic {
+      opacity: 1;
+      transform: translateY(0);
+    }
+    & .avatar__text {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+  &-leave {
+    opacity: 1;
+  }
+  &-leave-to {
+    opacity: 0;
+  }
+}
+
+.password {
+  &-enter-active {
+    transition: 1s;
+  }
+  &-enter {
+    opacity: 0;
+  }
+  &-enter-to {
+    opacity: 0.35;
+  }
+}
+
+
+
+@keyframes separator_search {
+  from {
+    bottom: -60px;
+  }
+  to {
+    bottom: 100%;
+  }
+}
 </style>
+
+
+// Загрузка имени
+// анимация на центральный элемент
+// посдветка ошибки
+// кнопка войти и далее
